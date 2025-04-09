be92306b325513ca6bb97a20da20a9789a55054b
Gustavo Martins Rodrigues <gustavomrodrigues11@gmail.com>
2025-04-09 10:58:04 -0300

    init commit

---
+++ src/main/java/com/unifor/estuda_facil/EstudaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.unifor.estuda_facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudaFacilApplication.class, args);
+	}
+
+}
