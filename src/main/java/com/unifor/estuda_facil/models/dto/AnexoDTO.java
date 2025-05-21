fa38204e486eb3adc5525a7c493918b21d9c5895
lucasximenes30 <lucasximenes177@gmail.com>
2025-05-21 09:26:20 -0300

    feat: implementação Factory em Anexo.

---
+++ src/main/java/com/unifor/estuda_facil/models/dto/AnexoDTO.java
@@ -0,0 +1,13 @@
+package com.unifor.estuda_facil.models.dto;
+
+import lombok.Data;
+
+@Data
+public class AnexoDTO {
+    private String nomeArquivo;
+    private String caminhoArquivo;
+    private String tipo;
+    private Long tamanho;
+    private Long tarefaId; // opcional
+    private Long avisoId;  // opcional
+}
